e98571a0f591c6f1fd1721014337e5f19bc6fcf7
eutawan <principalacc169@gmail.com>
2023-08-30 18:32:45 -0300

    questaoOnze
    minha décima primera questão

---
+++ src/questaoOnze.java
@@ -0,0 +1,9 @@
+public class questaoOnze {
+    public static void main(String[] args){
+
+        String fruta1 = "manga";
+        String fruta2 = "kiwi";
+
+        System.out.println("Eu gosto de " + fruta1 + " e " + fruta2);
+    }
+}
