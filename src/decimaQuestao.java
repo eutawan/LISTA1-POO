1a1b16460764fe0b739273852875f4f0f016528a
eutawan <principalacc169@gmail.com>
2023-08-30 18:27:12 -0300

    decimaQuestao
    minha decima questão

---
+++ src/decimaQuestao.java
@@ -0,0 +1,10 @@
+public class decimaQuestao {
+    public static void main(String[] args){
+
+        double saldo = 500.50;
+        double deposito = saldo + 300.25;
+        double saque = deposito - 150.75;
+
+        System.out.println(saque);
+    }
+}
