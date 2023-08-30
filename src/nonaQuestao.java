1690e1e0f96610eb841560d2dee36624939b715f
eutawan <principalacc169@gmail.com>
2023-08-30 18:21:27 -0300

    nonaQuestao
    minha nona questão

---
+++ src/nonaQuestao.java
@@ -0,0 +1,13 @@
+public class nonaQuestao {
+    public static void main(String[] args){
+
+        int numero1 = 14;
+        int numero2 = 14;
+
+        System.out.printf("a soma dos numeros: %d \n", numero1 + numero2);
+        System.out.printf("a subtraçao dos numeros: %d \n", numero1 - numero2);
+        System.out.printf("a multiplicaçao dos numeros: %d \n", numero1 * numero2);
+        System.out.printf("a divisao dos numeros: %d", numero1 / numero2);
+
+    }
+}
