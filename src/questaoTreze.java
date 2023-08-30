bdefec122e6649e3f2fdd8055bc89e73094b59a9
eutawan <principalacc169@gmail.com>
2023-08-30 18:47:22 -0300

    questaoTreze
    minha décima terceira questão

---
+++ src/questaoTreze.java
@@ -0,0 +1,13 @@
+public class questaoTreze {
+    public static void main(String[] args){
+
+        int numero = 11;
+
+        if (numero >= 0 & numero <= 10){
+            System.out.println("O número está entre 0 e 10");
+        }
+        else {
+            System.out.println("o numero nao esta entre 0 e 10");
+        }
+    }
+}
