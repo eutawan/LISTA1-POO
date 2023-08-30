a4f3acf2c9932562d971371989186c914bc87d1e
eutawan <principalacc169@gmail.com>
2023-08-30 18:39:01 -0300

    questaoDoze
    minha décima segunda questão

---
+++ src/questaoDoze.java
@@ -0,0 +1,8 @@
+public class questaoDoze {
+    public static void main(String[] args){
+
+        int idadeAnos = 19;
+
+        System.out.printf("a minha idade em meses: %d", idadeAnos * 12);
+    }
+}
