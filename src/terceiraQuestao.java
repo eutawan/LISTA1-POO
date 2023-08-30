0b2f9b0245220339b65c5e00b39896748d860239
eutawan <principalacc169@gmail.com>
2023-08-30 17:30:05 -0300

    terceiraQuestao
    minha terceira questão

---
+++ src/terceiraQuestao.java
@@ -0,0 +1,10 @@
+public class terceiraQuestao {
+    public static void main(String[] args) {
+
+        double num = 5.67;
+        double mult = num * 3;
+
+        System.out.println(mult);
+
+    }
+}
